9c965dc2ec96bd6c9a83c1cf5f20ada09c085603
Yongjun Jeon <isitea@isitea.net>
2018-12-21 00:29:38 +0900

    002

--- extension/scripts/Viewer.js
+++ extension/scripts/Viewer.js
@@ -14,16 +14,23 @@ function randomWrite( storage ) {
 let grabbler = new ImageGrabbler();
 
 class ComicGrabbler {
-    constructor ( Preset ) {
-        if ( Preset ) {
-            Object.defineProperty( this, "subject", { value: grabbler.analyseInformation( Preset ), writable: true, configurable: false } ) ;
-            let { title, subTitle } = grabbler.analyseInformation( Preset );
+    constructor ( Preset = {} ) {
+        Object.defineProperty( this, "subject", { writable: true, configurable: false } ) ;
+        if ( Preset.pattern ) {
+            this.subject = grabbler.analyseInformation( Preset.pattern );
         }
+        if ( this.subject !== null && Preset.storage ) {}
     }
 
-    saveToLocal () {
-        let zip = grabbler.solidateImages();
+    async saveToLocal ( { localPath, onConflict } ) {
+        let zip = await grabbler.solidateImages();
         zip.file( 'Downloaded from.txt', new Blob( [ document.URL ], { type: 'text/plain' } ) );
-        
+        let blob = await zip.generateAsync( { type: "blob" } );
+        return {
+            blob,
+            url: URL.createObjectURL( blob ),
+            filename: `${localPath}${this.subject.title}/${this.subject.subTitle}.zip`,
+            conflictAction: onConflict
+        }
     }
 }
